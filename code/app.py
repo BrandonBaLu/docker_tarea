c011741b49a40c81ea02f6e87a4f5706b7143988
BrandonBaLu <1719110177@utectulancingo.edu.mx>
2022-05-21 16:58:00 +0000

    jalo

---
+++ code/app.py
@@ -0,0 +1,27 @@
+from operator import index
+import web
+render= web.template.render("templates")
+urls = (
+    '/', 'index',
+    '/ubuntu', 'ubuntu',
+    '/docker', 'docker',
+)
+app = web.application(urls, globals())
+
+class index:
+    def GET(self):
+        return render.index()
+
+class ubuntu:
+    def GET(self):
+        return render.ubuntu()
+
+class docker:
+    def GET(self):
+        return render.docker()     
+        
+           
+if __name__ == "__main__":
+    app.run()
+
+
